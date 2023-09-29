c0a6e961c03477cb61133a62fd2deb9c16e7b795
souiai <sou.dali@gmail.com>
2023-09-29 10:35:48 +0200

    projet LITReview

--- LITReview/review/forms.py
+++ LITReview/review/forms.py
@@ -10,6 +10,7 @@ CHOICES = ((1, '-1'), (2, '-2'), (3, '-3'), (4, '-4'), (5, '-5'))
 class ReviewForm(forms.ModelForm):
     rating = forms.ChoiceField(required=True, choices=CHOICES,
                                widget=forms.RadioSelect(attrs={'class': 'Radio'}))  # ,initial=1
+    edit_review = forms.BooleanField(widget=forms.HiddenInput, initial=True)
 
     class Meta:
         model = review.models.Review
@@ -17,6 +18,8 @@ class ReviewForm(forms.ModelForm):
 
 
 class TicketForm(forms.ModelForm):
+    edit_ticket = forms.BooleanField(widget=forms.HiddenInput, initial=True)
+
     class Meta:
         model = review.models.Ticket
         fields = ['title', 'description', 'image']
@@ -42,5 +45,12 @@ class DeleteTicketForm(forms.Form):
 class DeleteReviewForm(forms.Form):
     delete_review = forms.BooleanField(widget=forms.HiddenInput, initial=True)
 
+
+"""______________________________________________________"""
+
+"""Ces champs utilisent le widget   HiddenInput   ,
+et ne seront pas vus par l’utilisateur sur le front-end.
+Le choix du type de champ et de la valeur initiale est quelque peu arbitraire,
+vu que nous allons simplement vérifier la présence du champ dans la vue."""
 
 
